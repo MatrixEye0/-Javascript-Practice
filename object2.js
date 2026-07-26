7dd3efa86cc43568e1e52f15f72ef3ce4a44055c
matrixeye0 <singhjagjit9735@gmail.com>
2026-07-26 18:06:54 +0530

    Destruct in object

---
+++ object2.js
@@ -0,0 +1,10 @@
+const user = {
+    name:"Jagjit",
+    lastname:"Singh",
+    age:22,
+    location:"San francisco",
+    workday:["monday","tuesday","wednesday","thursday","friday"] 
+ } 
+ const {location:u, age:a}=user
+ console.log(u,a); // destruct the objejct if you dont want call user again for return user obj values 
+ 
