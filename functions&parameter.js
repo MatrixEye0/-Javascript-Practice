7a58040422f0784b6218fb7b26990fd592051694
matrixeye0 <singhjagjit9735@gmail.com>
2026-07-27 12:45:53 +0530

    function practice 2

---
+++ functions&parameter.js
@@ -0,0 +1,20 @@
+function first(a,b){// here a,b is parameter
+
+    //    let sum = a+b
+    //    return sum
+    return a+b //both is ok 
+    
+}
+const result = first(5,8)// here 5,8 is argument
+const result2 = first(5,"a")
+const result3 = first(5, null)
+
+console.log("Result 1 : ", result)
+console.log("Result 2 : ", result2)
+console.log("Result 3 : ", result3)
+
+function loguser(user){
+    return`${user} just logged in`
+}
+console.log(loguser('Jagjit'));// if we pass nothing it return undefine 
+ 
