0012f0489a153710cffa234585568209d4755ca3
matrixeye0 <singhjagjit9735@gmail.com>
2026-07-29 16:21:05 +0530

    filter notes

--- filter.js
+++ filter.js
@@ -1,3 +1,6 @@
+
+// filter Create a new array with matching elements
+
 const arr =[1,2,3,4,,5,6,7,8,9,10]
 
 const test = arr.filter( (value)=> value>6)
@@ -8,3 +11,7 @@ const test2= arr.filter((val2)=>{
     return val2>4
 })
  console.log(test2)
+
+// forEach use when we want Printing, updating, API calls is return undefine.
+// Use forEach() when you want to do something with each element.
+// Use filter() when you want to keep only some elements based on a condition.
