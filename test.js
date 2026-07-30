6a9bf1d81f449c1221ef62921d9d202e0c626912
matrixeye0 <singhjagjit9735@gmail.com>
2026-07-30 21:10:53 +0530

    add notes

--- test.js
+++ test.js
@@ -2,9 +2,9 @@ const jagjit = 80809
 
 let email = "ab@gmail.com"
 var pass = "abc56" // var create problem in scope when we declare var inside or outside scope sso outside scope value run
-let check
+let check // undefine because not store anywhere
 
-city = "delhi"
+city = "Delhi"
 city="Asmoli"
 
 console.table([ jagjit, email, typeof pass, city, check])
