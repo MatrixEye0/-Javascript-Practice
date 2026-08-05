a0906e2f10a7ede3f91483083a61b93a85756e84
matrixeye0 <singhjagjit9735@gmail.com>
2026-08-05 13:01:46 +0530

    clean line extra space

--- fetch.js
+++ fetch.js
@@ -3,23 +3,18 @@
 
 fetch("https://jsonplaceholder.typicode.com/users/1")
   .then(response => response.json()) // Convert response to JSON
-  .then(data => {
-    console.log(data);
-  })
-  .catch(error => {
-    console.log("Error:", error);
-  });
+  .then(data => { console.log(data); })
+  .catch(error => { console.log("Error:", error); });
 
 //------------------in async and await style----------------------------------------------------------
 
 async function getUser() {
   try {
     const response = await fetch("https://jsonplaceholder.typicode.com/users/1");
-
     const data = await response.json();
-
     console.log(data);
-  } catch (error) {
+  } 
+  catch (error) {
     console.log("Error:", error);
   }
 }
