3b199005c183cdb61bbc95be7018097aab1cd652
matrixeye0 <singhjagjit9735@gmail.com>
2026-08-08 18:21:19 +0530

    leetcode problem 27

---
+++ leetcode27.js
@@ -0,0 +1,15 @@
+var removeElement = function(nums, val) {
+    let k = 0;
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] !== val) {
+            nums[k] = nums[i];
+            k++;
+        }
+    }
+    return k;
+};
+let nums = [2,3,4,5,2,3,2,9,2];
+let k = removeElement(nums, 2);
+
+console.log(k);                        
+console.log(nums.slice(0,k)); 
